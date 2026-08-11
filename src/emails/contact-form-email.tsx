0105f4cdee7a54b34322be4448237ca89a279ca1
Aron Hawkins <hawkins.aron@gmail.com>
2026-08-11 10:23:03 -0500

    add fields to contact form email

--- src/emails/contact-form-email.tsx
+++ src/emails/contact-form-email.tsx
@@ -1,17 +1,20 @@
-import * as React from 'react';
 
 interface EmailTemplateProps {
     name: string;
     email: string;
+    budget: string;
+    timeline: string;
     message: string;
 }
 
-export function ContactFormEmail({ name, email, message }: EmailTemplateProps) {
+export function ContactFormEmail({ name, email, budget, timeline, message }: EmailTemplateProps) {
     return (
         <div>
             <h1>You have received a new contact form submission</h1>
             <p>Name: {name}</p>
             <p>Email address: {email}</p>
+            <p>Budget: {budget}</p>
+            <p>Timeline: {timeline}</p>
             <p>Message: {message}</p>
         </div>
     );
